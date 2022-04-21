e8fe0ca88667a94be38f6c52c04671096e33f7da
wiktordziadosz <szymonwik99gmail.com>
2022-04-21 18:54:05 +0200

    pwa

--- gh-pages-vue-deployment/vue.config.js
+++ gh-pages-vue-deployment/vue.config.js
@@ -10,12 +10,13 @@ const { defineConfig } = require('@vue/cli-service')
             msTileColor: '#000000',
             appleMobileWebAppCapable: 'yes',
             appleMobileWebAppStatusBarStyle: 'black',
+            /*
             // configure the workbox plugin
             workboxPluginMode: 'InjectManifest',
             workboxOptions: {
             // swSrc is required in InjectManifest mode.
             swSrc: 'dev/sw.js',
             // ...other Workbox options...
-            }
+            }*/
         }
     })
