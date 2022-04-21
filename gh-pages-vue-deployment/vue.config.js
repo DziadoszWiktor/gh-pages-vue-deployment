12941fcde7cd22e725d41654a6ae4f92db99916b
wiktordziadosz <szymonwik99gmail.com>
2022-04-21 18:51:16 +0200

    pwa

--- gh-pages-vue-deployment/vue.config.js
+++ gh-pages-vue-deployment/vue.config.js
@@ -3,5 +3,19 @@ const { defineConfig } = require('@vue/cli-service')
         transpileDependencies: true,
         publicPath: process.env.NODE_ENV === 'production'
             ? '/gh-pages-vue-deployment/'
-            : '/'
+            : '/',
+        pwa: {
+            name: 'My App',
+            themeColor: 'red',
+            msTileColor: '#000000',
+            appleMobileWebAppCapable: 'yes',
+            appleMobileWebAppStatusBarStyle: 'black',
+            // configure the workbox plugin
+            workboxPluginMode: 'InjectManifest',
+            workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: 'dev/sw.js',
+            // ...other Workbox options...
+            }
+        }
     })
